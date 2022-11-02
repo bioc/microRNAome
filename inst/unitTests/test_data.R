a77aa6e023abf1c18ec11c0fe21f8bec88e6a724
mccallm <mccallm@gmail.com>
2022-11-02 14:21:12 -0400

    Update to version 3 of the microRNAome data.

--- inst/unitTests/test_data.R
+++ inst/unitTests/test_data.R
@@ -1,7 +1,7 @@
 test_data <- function(){
     data(microRNAome)
-    checkEqualsNumeric(dim(microRNAome), c(2546, 1312))
-    checkEqualsNumeric(dim(colData(microRNAome)), c(1312, 14))
-    checkEqualsNumeric(assay(microRNAome)[5,5], 246)
-    checkEquals(colnames(assay(microRNAome)), colData(microRNAome)$sample_id)
+    checkEqualsNumeric(dim(microRNAome), c(2731, 2406))
+    checkEqualsNumeric(dim(colData(microRNAome)), c(2406, 31))
+    checkEqualsNumeric(assay(microRNAome)[5,5], 5900)
+    checkEquals(colnames(assay(microRNAome)), colData(microRNAome)$Run)
 }
